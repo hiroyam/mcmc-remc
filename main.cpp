bd4a53bedaeaef360b226eb0b425759e438b3ee1
hiroyam <hiroyam@users.noreply.github.com>
2016-11-24 18:23:51 +0900

    Fix indent

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ float gauss_kernel(float x, float temperature) {
     float inverse_temperature = 1.0f / temperature;
 
     auto multimordal_gauss = [](float x) -> float {
-        return std::exp(-std::pow(x - 10, 2) / 2.0f) / std::sqrt(2.0f * M_PI) +
-               std::exp(-std::pow(x + 10, 2) / 2.0f) / std::sqrt(2.0f * M_PI);
-    };
+            return std::exp(-std::pow(x - 10, 2) / 2.0f) / std::sqrt(2.0f * M_PI) +
+                   std::exp(-std::pow(x + 10, 2) / 2.0f) / std::sqrt(2.0f * M_PI);
+        };
     return std::pow(multimordal_gauss(x), inverse_temperature);
 }
 
@@ -20,14 +20,12 @@ int main(int argc, char *argv[]) {
 
         // 温度の違う系を複数用意する
         const int n_systems = 3;
-        vec_t t {-10.0f, -10.0f, -10.0f};
-        vec_t temperatures {1.0f, 10.0f, 100.0f};
+        vec_t     t {-10.0f, -10.0f, -10.0f};
+        vec_t     temperatures {1.0f, 10.0f, 100.0f};
 
         for (int i = 0; i < n_loop; i++) {
-
             // それぞれの系で独立にMCMCを行う
             for (int j = 0; j < n_systems; j++) {
-
                 // ランダムウォークMH法
                 float a = t[j] + uniform_rand(-5.0f, 5.0f);
                 float r = gauss_kernel(a, temperatures[j]) / gauss_kernel(t[j], temperatures[j]);
@@ -43,12 +41,12 @@ int main(int argc, char *argv[]) {
             // 適当なステップ毎にランダムに選んだ系について確率min(1,r)で状態を交換する
             const int exchange_freq = 1;
             if (i % exchange_freq == 0) {
-                int j = uniform_rand(0, 1); // 0<->1 or 1<->2
-                float nu = gauss_kernel(t[j], temperatures[j+1]) * gauss_kernel(t[j+1], temperatures[j]);
-                float de = gauss_kernel(t[j], temperatures[j])   * gauss_kernel(t[j+1], temperatures[j+1]);
+                int   j  = uniform_rand(0, 1);                         // 0<->1 or 1<->2
+                float nu = gauss_kernel(t[j], temperatures[j + 1]) * gauss_kernel(t[j + 1], temperatures[j]);
+                float de = gauss_kernel(t[j], temperatures[j])   * gauss_kernel(t[j + 1], temperatures[j + 1]);
                 float r  = nu / de;
                 if (std::min(1.0f, r) > uniform_rand(0.0f, 1.0f)) {
-                    std::swap(t[j], t[j+1]);
+                    std::swap(t[j], t[j + 1]);
                 }
             }
         }
@@ -88,9 +86,7 @@ int main(int argc, char *argv[]) {
         // for (auto v : correlations) {
         //     ofs << format_str("%f", v) << std::endl;
         // }
-
     } catch (const std::exception &e) {
         std::cerr << colorant('y', format_str("error: %s", e.what())) << std::endl;
     }
 }
-
